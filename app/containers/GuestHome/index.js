b579a8c801ff789cafe33a31f50153483cafc3c6
faiyaz <m.faiyaz123@gmail.com>
2019-02-27 14:00:21 +0530

    added guest home header

--- app/containers/GuestHome/index.js
+++ app/containers/GuestHome/index.js
@@ -8,10 +8,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { Route } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import { Switch } from 'react-router-dom';
+import { Switch,Route } from 'react-router-dom';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 import AdminTask from 'containers/AssignAdminTask/Loadable';
@@ -25,13 +24,11 @@ import AboutUs from 'containers/AboutUs/Loadable';
 import PrivateRoute from 'components/PrivateRoute/Loadable';
 import CenterMenus from 'components/CenterMenus/Loadable';
 import About from '../UserProfile/components/About';
+import AppBar from './AppBar'
 
-import {
-  makeSelectLoggedUserHomeMenus,
-} from '../../store/loggeduser/selectors';
+import { makeSelectLoggedUserHomeMenus } from '../../store/loggeduser/selectors';
 import reducer from './reducer';
 import saga from './saga';
-
 
 /* eslint-disable react/prefer-stateless-function */
 export class GuestHome extends React.PureComponent {
@@ -42,28 +39,25 @@ export class GuestHome extends React.PureComponent {
           <title>Home</title>
           <meta name="description" content="Description of GuestHome" />
         </Helmet>
-      <React.Fragment>
-        <Helmet>
-          <title>Home</title>
-          <meta name="description" content="Description of Home" />
-        </Helmet>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => <CenterMenus menus={this.props.homeMenus} />}
-          />
-          <PrivateRoute path="/helpline" component={Helpline} />
-          <PrivateRoute path="/admintaskassignment" component={AdminTask} />
-          <PrivateRoute path="/my/admintasks" component={MyAdminTasks} />
-          <PrivateRoute path="/news/trends" component={TrendingNews} />
-          <PrivateRoute path="/ngos" component={Ngo} />
-          <PrivateRoute path="/profile/:userId/about" component={About} />
-          <PrivateRoute path="/profile/:userId" component={Profile} />
-          <PrivateRoute path="/support" component={Support} />
-          <PrivateRoute path="/about" component={AboutUs} />
-        </Switch>
-      </React.Fragment>
+        <React.Fragment>
+         <AppBar/>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={() => <CenterMenus menus={this.props.homeMenus} />}
+            />
+            <PrivateRoute path="/helpline" component={Helpline} />
+            <PrivateRoute path="/admintaskassignment" component={AdminTask} />
+            <PrivateRoute path="/my/admintasks" component={MyAdminTasks} />
+            <PrivateRoute path="/news/trends" component={TrendingNews} />
+            <PrivateRoute path="/ngos" component={Ngo} />
+            <PrivateRoute path="/profile/:userId/about" component={About} />
+            <PrivateRoute path="/profile/:userId" component={Profile} />
+            <PrivateRoute path="/support" component={Support} />
+            <PrivateRoute path="/about" component={AboutUs} />
+          </Switch>
+        </React.Fragment>
       </div>
     );
   }
