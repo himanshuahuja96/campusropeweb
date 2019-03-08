c3fa9f93bdb50e3c4976677222101264db3b87b2
faiyaz <m.faiyaz123@gmail.com>
2019-03-08 22:18:16 +0530

    removed unused components

--- app/components/CenterMenus/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for CenterMenus
- *
- */
-
-import loadable from 'loadable-components';
-
-export default loadable(() => import('./index'));
--- app/components/Content/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for Content
- *
- */
-
-import loadable from 'loadable-components';
-
-export default loadable(() => import('./index'));
